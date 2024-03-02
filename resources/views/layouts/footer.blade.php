26f76a2fcf8790b3ee4dab24c43915932eb6c427
e.bibaj <e.bibaj@gmail.com>
2024-03-02 17:38:29 +0100

    Updates

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="footer">
 <!-- Log on to codeastro.com for more projects! -->
-     ©  {{date('Y')}} Simple Attendance Management System <span class="d-none d-sm-inline-block"> - Crafted with <i class="mdi mdi-heart text-danger"></i> by Ali Aqa Atayee</span>.
+     ©  {{date('Y')}} Sistemi i Menaxhimit të Pjesëmarrjes <span class="d-none d-sm-inline-block"> - Zhvilluar me <i class="mdi mdi-heart text-danger"></i> nga 4P Dev</span>.
 </footer>
