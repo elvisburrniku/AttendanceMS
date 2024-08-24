5a52fbab7e299a11c0df56c1642be2f91756fef9
Elvis Burrniku <elvis.burrniku.ext@conroo.com>
2024-08-24 15:28:30 +0200

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
 
     public function employee()
     {
-        return $this->belongsTo('App\Models\Employee', 'email', 'nickname');
+        return $this->belongsTo('App\Models\Employee', 'email', 'email');
     }
 
     public function getRouteKeyName()
